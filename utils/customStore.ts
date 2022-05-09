e013391f5f49d42a4d1d5ecc51160d946f0de2a6
Chukwuzube Samuel <vansomecsam@gmail.com>
2022-05-09 01:48:54 +0100

    Create customStore.ts

---
+++ utils/customStore.ts
@@ -0,0 +1,27 @@
+// import reducer from './bugs';
+
+type Listener = () => void;
+
+function createStore<T, A>(reducer: (state: T, action: A) => T) {
+  let state: T;
+  let listeners: Listener[] = [];
+
+  return {
+    getState() {
+      return state;
+    },
+    dispatch(action: A) {
+      state = reducer(state, action);
+      listeners.forEach((listener) => listener());
+    },
+    subscribe(listener: Listener) {
+      listeners.push(listener);
+    },
+  };
+}
+
+// export default createStore(reducer);
+
+// Middleware configuration when not using reduxjs-toolkit
+// import { applyMiddleware, createStore } from 'redux';
+// const store = createStore(reducer, applyMiddleware(logger))
